38f7998be762985d1ae4ab676a6950c7ab49e9d1
Moshoeshoe Mokhele <mmokhele0@gmail.com>
2021-01-13 16:50:07 +0200

    New Business Changes

---
+++ Funeral.Model/NewBusinessModel.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Funeral.Model
+{
+    public class NewBusinessModel
+    {
+        public NewBusinessModel()
+        {
+
+            CreateDate = System.DateTime.Now;
+            MemberType = string.Empty;
+            Title = string.Empty;
+            FullNames = string.Empty;
+            Surname = string.Empty;
+            Gender = string.Empty;
+            IDNumber = string.Empty;
+            DateOfBirth = System.DateTime.Now;
+            Telephone = string.Empty;
+            Cellphone = string.Empty;
+            Address1 = string.Empty;
+            Address2 = string.Empty;
+            Address3 = string.Empty;
+            Address4 = string.Empty;
+            Code = string.Empty;
+            //New data feilds [Tab 2] - Postal Address
+            Address1_Post = string.Empty;
+            Address2_Post = string.Empty;
+            Address3_Post = string.Empty;
+            Address4_Post = string.Empty;
+            Code_Post = string.Empty;
+            //end
+            MemeberNumber = string.Empty;
+            MemberSociety = string.Empty;
+            fkiPlanID = 1;
+            Active = true;
+            InceptionDate = System.DateTime.Now;
+            Claimnumber = string.Empty;
+            PolicyStatus = string.Empty;
+            //   parlourid =Convert.tou string.Empty;
+            Agent = string.Empty;
+            AccountHolder = string.Empty;
+            Bank = string.Empty;
+            BranchCode = string.Empty;
+            Branch = string.Empty;
+            AccountNumber = string.Empty;
+            AccountType = string.Empty;
+            DebitDate = System.DateTime.Now;
+            MemberBranch = string.Empty;
+            CoverDate = System.DateTime.Now;
+            LastModified = System.DateTime.Now;
+            ModifiedUser = string.Empty;
+            Email = string.Empty;
+            Passport = string.Empty;
+            Citizenship = string.Empty;
+            EasyPayNo = string.Empty;
+            PlanName = string.Empty;
+            pkiAdditionalMemberInfo = new Guid("00000000-0000-0000-0000-000000000000");
+            StartDate = System.DateTime.Now;
+            Administratorparlourid = new Guid("00000000-0000-0000-0000-000000000000");
+
+        }
+        public int pkiMemberID { get; set; }
+        public DateTime CreateDate { get; set; }
+        public string MemberType { get; set; }
+        public string Title { get; set; }
+        public int Age { get; set; }
+
+        [Required(ErrorMessage = "Please enter full name")]
+        [RegularExpression(@"[a-zA-Z ]*$", ErrorMessage = "full name Enter Only characters")]
+        public string FullNames { get; set; }
+        public int SocietyId { get; set; }
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Please enter Surname")]
+        [RegularExpression(@"[a-zA-Z ]*$", ErrorMessage = "Surname Enter Only characters")]
+        public string Surname { get; set; }
+
+        public string Gender { get; set; }
+
+        //[Required(ErrorMessage = "Please enter id number")]
+        //[ValidateId()]
+        public string IDNumber { get; set; }
+
+        public double TotalPremium { get; set; }
+
+        public string Province { get; set; }
+
+        [Required(ErrorMessage = "Please enter premium amount")]
+        public string PolicyPremium { get; set; }
+        public string CoverAmount { get; set; }
+        public string PolicyNumber { get; set; }
+
+        [Required(ErrorMessage = "Please select product name")]
+        public int ProductId { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        [RegularExpression(@"^[0-9]*$", ErrorMessage = "Telephone Number Enter Only Number")]
+        public string Telephone { get; set; }
+
+        //[Required(ErrorMessage = "Enter Cellphone Number")]
+        //[RegularExpression(@"^[0-9]+$", ErrorMessage = "Cellphone Number Enter Only Number")]
+        public string Cellphone { get; set; }
+
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string Address3 { get; set; }
+        public string Address4 { get; set; }
+        public string Code { get; set; }
+        //New Data - [Tab 2] Address
+        public string Address1_Post { get; set; }
+        public string Address2_Post { get; set; }
+        public string Address3_Post { get; set; }
+        public string Address4_Post { get; set; }
+        public string Code_Post { get; set; }
+        //end
+        public string MemeberNumber { get; set; }
+
+        public string MemberSociety { get; set; }
+        public int fkiPlanID { get; set; }
+        public bool Active { get; set; }
+
+        [Required(ErrorMessage = "Please enter Inception date")]
+        public DateTime InceptionDate { get; set; }
+        public string Claimnumber { get; set; }
+        public string PolicyStatus { get; set; }
+        public Guid parlourid { get; set; }
+        public Guid Administratorparlourid { get; set; }
+        public string Agent { get; set; }
+        public string AccountHolder { get; set; }
+        public string Bank { get; set; }
+        public string BranchCode { get; set; }
+        public string Branch { get; set; }
+        public string AccountNumber { get; set; }
+        public string AccountType { get; set; }
+        public DateTime DebitDate { get; set; }
+
+        [Required(ErrorMessage = "Select a Branch")]
+        public string MemberBranch { get; set; }
+        public DateTime? CoverDate { get; set; }
+        public DateTime LastModified { get; set; }
+        public string ModifiedUser { get; set; }
+
+        [RegularExpression(@"\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*", ErrorMessage = "Please enter valid email address")]
+        public string Email { get; set; }
+        public string Citizenship { get; set; }
+
+        //[Required(ErrorMessage = "Enter Passport Number")]
+        public string Passport { get; set; }
+        public string EasyPayNo { get; set; }
+        public Guid pkiAdditionalMemberInfo { get; set; }
+        public string PlanName { get; set; }
+
+        [Required(ErrorMessage = "Please enter Start date")]
+        public DateTime StartDate { get; set; }
+        public int CustomId1 { get; set; }
+        public int CustomId2 { get; set; }
+        public int CustomId3 { get; set; }
+        public int FK_MemberId { get; set; }
+        public int ClaimExistCount { get; set; }
+        public string ApplicationName { get; set; }
+    }
+}
---
+++ Funeral.Web/Areas/Admin/Controllers/NewBusinessController.cs
@@ -0,0 +1,104 @@
+﻿using Funeral.BAL;
+using Funeral.Model;
+using Funeral.Model.Search;
+using Funeral.Web.App_Start;
+using Funeral.Web.Areas.Admin.Models.ViewModel;
+using Funeral.Web.Common;
+using Microsoft.Reporting.WebForms;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+
+namespace Funeral.Web.Areas.Admin.Controllers
+{
+    public class NewBusinessController : BaseAdminController
+    {
+
+        private readonly ISiteSettings _siteConfig = new SiteSettings();
+        public static string GetdataPremium;
+
+        public NewBusinessController() : base(76)
+        {
+            this.dbPageId = 76;
+        }
+
+        [PageRightsAttribute(CurrentPageId = 76)]
+        public ActionResult Index()
+        {
+            ViewBag.HasAccess = HasAccess;
+            if (ViewBag.HasAccess == true)
+            {
+                var statusList = CommonBAL.GetStatus(FuneralEnum.StatusAssociatedTable.Members.ToString()).Select(x => new SelectListItem() { Text = x.ID.ToString(), Value = x.Status });
+                ViewBag.StatusList = statusList;
+                ViewBag.HasEditRight = HasEditRight;
+                ViewBag.HasDeleteRight = HasDeleteRight;
+                ViewBag.totalPremium = Currency;
+                ViewBag.SocietyLists = CommonBAL.GetSocietyByParlourId(CurrentParlourId);
+                LoadStatus();
+                LoadEntriesCount();
+                BindCompanyList("Search");
+                Model.Search.MemberSearch search = new Model.Search.MemberSearch();
+                search.CompanyId = new Guid(CurrentParlourId.ToString());
+                search.PageNum = 1;
+                search.PageSize = 10;
+                search.SarchText = "";
+                search.SortBy = "";
+                search.SortOrder = "Asc";
+                search.StatusId = "0";
+                search.TotalRecord = 0;
+                search.BookID = "";
+
+                var searchResult = new Funeral.Model.SearchResult<Model.Search.MemberSearch, MembersModel>(search, new List<MembersModel>(), o => o.IDNumber.Contains(search.SarchText)
+                     || o.MemeberNumber.Contains(search.SarchText));
+                return View(search);
+            }
+            else
+            {
+                return View();
+            }
+
+        }
+
+        public void LoadStatus()
+        {
+            var statusList = CommonBAL.GetStatus(FuneralEnum.StatusAssociatedTable.Members.ToString()).Select(status => new { status.Status, status.ID }).Distinct();
+            ViewBag.Statuses = statusList;
+        }
+        public void LoadEntriesCount()
+        {
+            List<KeyValue> keyValues = new List<KeyValue>();
+            List<SelectListItem> entriesItems = new List<SelectListItem>();
+            keyValues.Add(new KeyValue { Key = "10", Value = "10" });
+            keyValues.Add(new KeyValue { Key = "20", Value = "20" });
+            keyValues.Add(new KeyValue { Key = "25", Value = "25" });
+            keyValues.Add(new KeyValue { Key = "50", Value = "50" });
+            keyValues.Add(new KeyValue { Key = "100", Value = "100" });
+            keyValues.Add(new KeyValue { Key = "200", Value = "200" });
+            keyValues.Add(new KeyValue { Key = "250", Value = "250" });
+            keyValues.Add(new KeyValue { Key = "500", Value = "500" });
+            ViewBag.EntriesCount = keyValues;
+        }
+        [HttpPost]
+        public ActionResult SearchData(Model.Search.MemberSearch search)
+        {
+            var searchResult = new Funeral.Model.SearchResult<Model.Search.MemberSearch, MembersModel>(search, new List<MembersModel>(), o => o.IDNumber.ToLower().Contains(search.SarchText.ToLower()));
+            try
+            {
+                var members = MembersBAL.GetAllMembers(search.CompanyId, search.PageSize, search.PageNum, search.SarchText, search.SortBy, search.SortOrder, search.StatusId.ToString(), search.BookID);
+                return Json(new Funeral.Model.SearchResult<Model.Search.MemberSearch, MembersModel>(search, members.MemberList, o => o.IDNumber.ToLower().Contains(search.SarchText.ToLower()) || o.MemeberNumber.ToLower().Contains(search.SarchText.ToLower()) || o.Surname.ToLower().Contains(search.SarchText.ToLower()) || o.FullNames.ToLower().Contains(search.SarchText.ToLower()) || o.Cellphone.ToLower().Contains(search.SarchText.ToLower()) || o.EasyPayNo.ToLower().Contains(search.SarchText.ToLower())));
+            }
+            catch (Exception ex)
+            {
+                return Json(WebApiResult<Model.Search.MemberSearch, MembersModel>.Error(searchResult, ex));
+            }
+        }
+    }
+}
