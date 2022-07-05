1c98f31fd61013d61f092d32425d0dc9b535409b
Moshoeshoe Mokhele <mmokhele0@gmail.com>
2022-07-05 13:57:42 +0200

    Mo Changes

--- Funeral.Model/Search/BaseSearch.cs
+++ Funeral.Model/Search/BaseSearch.cs
@@ -17,5 +17,13 @@ namespace Funeral.Model.Search
         public string SarchText { get; set; }
         public Guid CompanyId { get; set; }
 
+        public string ClaimID { get; set; }
+        public DateTime DateFrom { get; set; }
+        public DateTime DateTo { get; set; }
+        public string StatusId { get; set; }
+        public string SocietyID { get; set; }
+        public string keyword { get; set; }
+        public string SearchType { get; set; }
+
     }
 }
