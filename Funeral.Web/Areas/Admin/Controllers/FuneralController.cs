a31a4eaea09e4a1aad3b7dc6d31e43733075bb80
Hemant Yadav <truecodemaster@gmail.com>
2022-08-26 17:21:12 +0530

    Funeral detail page implemented
    Funeral detail page implemented

--- Funeral.Web/Areas/Admin/Controllers/FuneralController.cs
+++ Funeral.Web/Areas/Admin/Controllers/FuneralController.cs
@@ -572,12 +572,14 @@ namespace Funeral.Web.Areas.Admin.Controllers
                 return View(funeralModel);
 
             funeralModel = FuneralBAL.SelectFuneralByFuneralId(funeralId.GetValueOrDefault(0), this.ParlourId);
+            funeralModel.FuneralDocuments = FuneralBAL.SelectFuneralDocumentsByMemberId(funeralModel.pkiFuneralID);
             return View(funeralModel);
         }
 
         [HttpPost]
         public ActionResult FuneralServices(FuneralModel model, string submitForm)
         {
+            string savedTabConfirmationMsg = model.Status == "New" ? "BodyCollection " : model.Status + " saved successfully";
             if (ModelState.IsValid)
             {
                 model.parlourid = this.ParlourId;
@@ -588,6 +590,9 @@ namespace Funeral.Web.Areas.Admin.Controllers
                     model.Status = GetStatus(model.Status, submitForm);
                 model.pkiFuneralID = FuneralBAL.SaveFuneral(model);
             }
+            if (!string.IsNullOrEmpty(savedTabConfirmationMsg))
+                TempData["savedTabConfirmationMsg"] = savedTabConfirmationMsg;
+            model.FuneralDocuments = FuneralBAL.SelectFuneralDocumentsByMemberId(model.pkiFuneralID);
             return View(model);
         }
         public ActionResult FuneralSearch()
